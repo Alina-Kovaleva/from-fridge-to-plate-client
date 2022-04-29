66b516cb278c94acc7f94d1efe628c8abe749112
Alina Kovaleva <calipco.xp@gmail.com>
2022-04-29 14:47:01 +0200

    fix config app url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
